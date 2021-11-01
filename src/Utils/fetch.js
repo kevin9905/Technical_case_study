571d147b3a2f292940ae7757341616532ebdf3c2
kevin9905 <kevinphamcao@gmail.com>
2021-11-01 13:42:29 -0400

    CORS problem solved

--- src/Utils/fetch.js
+++ src/Utils/fetch.js
@@ -1,10 +1,9 @@
 export const getComic = async (num) => {
-  const isLatest = isNaN(num);
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  //const isLatest = isNaN(num);
   //const url = "https://xkcd.vercel.app/?comic=" + (!isLatest ? num : "latest");
-  const url = "https://xkcd.com/" + (num ? num + "/" : "") + "info.0.json";
+  const url = "/" + (num ? num + "/" : "") + "info.0.json";
 
-  const response = await fetch(proxyUrl + url);
+  const response = await fetch(url);
   const comics = await response.json();
   return comics;
 };
