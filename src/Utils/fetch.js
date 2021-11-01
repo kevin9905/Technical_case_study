9e49939d044440c939d20d30a3c5466c13488874
kevin9905 <kevinphamcao@gmail.com>
2021-11-01 02:22:15 -0400

    update fetch

--- src/Utils/fetch.js
+++ src/Utils/fetch.js
@@ -4,7 +4,7 @@ export const getComic = async (num) => {
   //const url = "https://xkcd.vercel.app/?comic=" + (!isLatest ? num : "latest");
   const url = "https://xkcd.com/" + (num ? num + "/" : "") + "info.0.json";
 
-  const response = await fetch(url);
+  const response = await fetch(proxyUrl + url);
   const comics = await response.json();
   return comics;
 };
