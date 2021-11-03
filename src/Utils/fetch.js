8e21aa7c78542455fddc39cf8d058feae558c217
kevin9905 <kevinphamcao@gmail.com>
2021-11-03 12:03:10 -0400

    add some comment

--- src/Utils/fetch.js
+++ src/Utils/fetch.js
@@ -1,6 +1,8 @@
 export const getComic = async (num) => {
   const isLatest = isNaN(num);
+  //this url works in server
   const url = "https://xkcd.vercel.app/?comic=" + (!isLatest ? num : "latest");
+  //this url does not work in server but works in localhost, blocks by CORS header
   //const url = "/" + (num ? num + "/" : "") + "info.0.json";
   //const url = "/info.0.json";
 
